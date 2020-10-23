314472bb394fd3e16c8cf9ffaa1960b5c789ce31
Shinya Kumagai <roomful.rooms@gmail.com>
2020-10-23 19:54:10 +0900

    WIP: Add Repository

--- Sources/Data/Repository.swift
+++ Sources/Data/Repository.swift
@@ -5,4 +5,21 @@
 //  Created by Shinya Kumagai on 2020/10/23.
 //
 
-import Foundation
+import Combine
+
+struct ListData: Identifiable {
+    let id: String
+    let text: String
+}
+
+struct DetailData: Identifiable {
+    let id: String
+    let text: String
+}
+
+protocol Repository {
+    
+    func getList() -> AnyPublisher<[ListData], Never>
+    
+    func getDetail(by id: Int) -> AnyPublisher<DetailData, Never>
+}
